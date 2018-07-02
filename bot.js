743d96ce086bbe0d5a78ee79b567630055bbfd48
cracraftyfty <40735864+cracraftyfty@users.noreply.github.com>
2018-07-02 07:55:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,31 @@ client.on('message', message => {
   	}
 });
 
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+  if(!args[0]) return message.channel.send("no");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+});
+
+}
+
+module.exports.help = {
+  name: "clear"
+}
+
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+}
+
+module.exports.help = {
+  name: "say"
+}
+
 client.login(process.env.BOT_TOKEN);
